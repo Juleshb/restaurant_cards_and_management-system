8fdcedb14983e10f06e8272901bfb73d31238109
juleshb <habaruremajules@gmail.com>
2023-03-06 15:46:50 +0200

    home page back end

--- nav.php
+++ nav.php
@@ -11,19 +11,79 @@ if (!isset($_SESSION["Email"])) {
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
 
-<html>
   <head>
-    
-    <title>Welcome to My Landing Page</title>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <title>Restorent cards and management system</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-onix-digital.css">
+    <link rel="stylesheet" href="assets/css/animated.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+<!--
+
+TemplateMo 565 Onix Digital
+
+https://templatemo.com/tm-565-onix-digital
+
+-->
   </head>
-  <body>
-  <div><a href="home.php">Home</a>
-  <a  href="customer.php">Customers</a>
-  <a href="payment.php">Payment</a>
-  <a href="#news">price</a>
-  <a href="updateadimin.php">Profile</a>
-  <a href="logout.php" class="logou">Logout</a>
-  <a href="logout.php" class="logou"><?php echo "Hey 🙌 " . $_SESSION["Email"];?></a>
- 
-</div>
+
+<body>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <nav class="main-nav">
+            <!-- ***** Logo Start ***** -->
+            <a href="index.html" class="logo">
+              <img src="assets/images/logo.png">
+            </a>
+            <!-- ***** Logo End ***** -->
+            <!-- ***** Menu Start ***** -->
+            <ul class="nav">
+              <li class="scroll-to-section"><a href="home.php">Home</a></li>
+              <li class="scroll-to-section"><a href="payment.php">Payment</a></li>
+              <li class="scroll-to-section"><a href="#news">price</a></li>
+              <li class="scroll-to-section"><a href="updateadimin.php">Profile</a></li>
+              <li class="scroll-to-section"><a href="logout.php" class="logou">Logout</a></li> 
+              <li class="scroll-to-section"><div class="main-red-button-hover"><a href=""><?php echo "Hey 🙌 " . $_SESSION["Email"];?></a></div></li> 
+            </ul>        
+            <a class='menu-trigger'>
+                <span>Menu</span>
+            </a>
+            <!-- ***** Menu End ***** -->
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
--- totalamount.php
+++ totalamount.php
@@ -67,7 +67,33 @@ $row2 = $stmt2->fetch(PDO::FETCH_ASSOC);
         
       </form>
     </div>
+<!-- Scripts -->
+<script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/js/owl-carousel.js"></script>
+  <script src="assets/js/animation.js"></script>
+  <script src="assets/js/imagesloaded.js"></script>
+  <script src="assets/js/custom.js"></script>
 
+  <script>
+  // Acc
+    $(document).on("click", ".naccs .menu div", function() {
+      var numberIndex = $(this).index();
+
+      if (!$(this).is("active")) {
+          $(".naccs .menu div").removeClass("active");
+          $(".naccs ul li").removeClass("active");
+
+          $(this).addClass("active");
+          $(".naccs ul").find("li:eq(" + numberIndex + ")").addClass("active");
+
+          var listItemHeight = $(".naccs ul")
+            .find("li:eq(" + numberIndex + ")")
+            .innerHeight();
+          $(".naccs ul").height(listItemHeight + "px");
+        }
+    });
+  </script>
     
 </body>
 </html>
